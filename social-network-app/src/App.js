89759013c310b03cb68ef201aee75c35384eac33
gabygabylagaby <gabrielagv7656@gmail.com>
2024-10-06 16:31:45 -0400

    feat: implement settings

--- social-network-app/src/App.js
+++ social-network-app/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
-import Home from './app/components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../src/app/pages/Home';
+import EditProfile from '../src/app/pages/EditProfile';
 
 function App() {
   return (
-    <Home />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/EditProfile" element={<EditProfile />} />
+      </Routes>
+    </Router>
   );
 }
 
